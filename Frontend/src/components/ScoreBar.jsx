cecd49c9b0a3ab7f1f7dc41898a7811a5e0e3040
Lamia Hamdi <101535283+Lamia1406@users.noreply.github.com>
2025-01-04 22:47:10 +0100

    change scorebar design

--- Frontend/src/components/ScoreBar.jsx
+++ Frontend/src/components/ScoreBar.jsx
@@ -1,8 +1,11 @@
-import MeatThigh from "../assets/icons/meat-thigh";
-
+import Lightning from "../assets/icons/lightning"
 export default function ScoreBar(){
-    return <div className="score-bar flex gap-2 items-center">
-        <p className="text-[#EF314C] text-2xl">00</p>
-        <MeatThigh/>
+    return <div className="score-bar w-[140px] justify-center relative flex text-center  h-[50px] px-3 rounded-[4px] bg-[#222525] items-center"
+    style={{
+        boxShadow: "0px 8px 4px 0px rgba(34, 37, 37, 0.75)"
+    }}>
+        <Lightning className={"absolute -left-4 -top-2 w-auto h-[66px]"}/>
+        <p className="text-white text-2xl font-[Rajdhani] font-bold">00</p>
+       
     </div>
 }
