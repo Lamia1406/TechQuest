9ee8114d633f4f171511e5967de8698c412053f4
Lamia Hamdi <101535283+Lamia1406@users.noreply.github.com>
2025-01-04 18:34:55 +0100

    add conditional rendering of authentication based pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,21 +5,41 @@ import Achievements from './pages/Achievements'
 import Profile from './pages/Profile'
 import Levels from './pages/Levels'
 import Learning from './pages/Learning'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+import { useEffect, useState } from 'react'
+import { supabase } from './supabase'
+import LandingPage from './pages/LandingPage'
 
 function App() {
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((_, session) => {
+      setSession(session)
+    })
+
+   
+  }, [])
 
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/home" element={<Home />}/>
-        <Route path="/achievements" element={<Achievements />} />
-        <Route path="/learning" element={<Learning />} />
-        <Route path="/levels" element={<Levels />} />
-        <Route path="/profile" element={<Profile />} />
-     
-    </Routes>
-  </BrowserRouter>
+      <Routes>
+        <Route path="/" element={!session ? <LandingPage /> : <Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+
+        {session && (
+          <>
+            <Route path="/home" element={<Home />} />
+            <Route path="/achievements" element={<Achievements />} />
+            <Route path="/learning" element={<Learning />} />
+            <Route path="/levels" element={<Levels />} />
+            <Route path="/profile" element={<Profile />} />
+          </>
+        )}
+      </Routes>
+    </BrowserRouter>
   )
 }
 
